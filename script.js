8ebcc470ed72af9e898f50bb408d1b928e5afd72
aahmad1123 <ayat.asaad@gmail.com>
2024-01-22 12:26:13 -0600

    Added more questions

--- script.js
+++ script.js
@@ -25,6 +25,21 @@ var quizQuestions = [
       title: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store:",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within what when being assigned to variables:",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very usefuol tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
     }
   ]
 
